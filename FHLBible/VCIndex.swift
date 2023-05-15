57688880a78576bda791823a12854d3fcbba346f
EmmanuelLo <snowray712000@gmail.com>
2023-05-15 17:49:38 +0800

    # 發佈2.7.1 - Bug: 舊約的parsing的分析，離線版會是空的。 - Bug: SN顯示切換，卻沒效果，要切換至下一章上一章才會顯示出來(離線版才會)。 - Bug: 多譯本顯示時，在太18會出誤，若同時用和合本、ESV譯本時，因為ESV沒有18v11節。 - 優化: 注釋排版，將多餘的空白移除。

--- FHLBible/VCIndex.swift
+++ FHLBible/VCIndex.swift
@@ -19,8 +19,8 @@ class VCIndex : UIViewController {
         super.viewDidLoad()
         _ = AutoLoadDUiabv.s // lazy var
         
-        labelDate.text = "2023.05.03a"
-        labelVer.text = "2.5.1" //version
+        labelDate.text = "2023.05.15a"
+        labelVer.text = "2.7.1" //version
      
     }
     
