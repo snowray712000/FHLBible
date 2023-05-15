27fd2c5fb618c640964663d57271c325c0bd03dc
EmmanuelLo <snowray712000@gmail.com>
2023-05-15 17:34:25 +0800

    # 注釋排版_多餘的空白 - 原始資料有換行，例如耶`換行`，`空白空白空白空白空白`穌 。

--- FHLBible/FHLTools/DictTwcbNewConvertor.swift
+++ FHLBible/FHLTools/DictTwcbNewConvertor.swift
@@ -86,9 +86,45 @@ class DictCbolNewEnConvertor : IStr2DTextArray {
         return r1
     }
 }
+
+fileprivate func removeAdditionalSpace(_ str:String)->String{
+    // 每行，若以下面任何作為字首，則處理空白數量；反之，將其加在上面一行
+    let itemData =  NSLocalizedString("零、,壹、,貳、,參、,肆、,伍、,陸、,柒、,捌、,玖、,拾,一、,二、,三、,四、,五、,六、,七、,八、,九、,十、,（一）,（二）,（三）,（四）,（五）,（六）,（七）,（八）,（九）,（十,1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.,13.,14.,15.,16.,17.,18.,19.,20.,(1,(2,(3),(4),(5),(6),(7),(8),(9),●,◎,┌,│,├,└,☆,○,a.,b.,c.,d.,e.,f.,g.,h.,i.,j.,k.,A.,B.,C.,D.,E.,F.,G.,H.,I.,J.,K.", comment: "").components(separatedBy: ",")
+    
+    // 空白隔開
+    var dataArray = str.components(separatedBy: "\r\n")
+    if dataArray.count == 0 { return str }
+    
+    // 假設: 第1行的空白數量一定是最少的，下面縮排，都減掉這個空白
+    let leadingWhitespaceCount0 = dataArray[0].prefix(while: { $0.isWhitespace }).count
+
+    // 從後面到前面，因為會 remove 某行，反過來處理較簡單
+    for i in ijnRange(dataArray.count - 1,dataArray.count,-1 ){
+        let lineStr = dataArray[i]
+        let trimmedStr = lineStr.trimmingCharacters(in: .whitespaces)
+        if trimmedStr.isEmpty { continue }
+        if sinq(itemData).any({trimmedStr.hasPrefix($0)}){
+            let leadingWhitespaceCount = lineStr.prefix(while: { $0.isWhitespace }).count
+            if leadingWhitespaceCount > leadingWhitespaceCount0 {
+                let startIndex = lineStr.index(lineStr.startIndex, offsetBy: leadingWhitespaceCount0)
+                    dataArray[i] = String(lineStr[startIndex...])
+            }
+        } else {
+            if i != 0 {
+                dataArray[i-1].append(trimmedStr)
+                dataArray.remove(at: i)
+            }
+        }
+    }
+    
+    return dataArray.joined(separator: "\r\n")
+}
+
 class CommentDataStrToDText : IStr2DTextArray{
     func main(_ str:String) -> [DText] {
-        var r1 = [DText(str)]
+        let str2 = removeAdditionalSpace(str)
+        
+        var r1 = [DText(str2)]
         r1 = ssDtParentheses(r1)
         r1 = ssDtReference(r1)
         r1 = ssDtNewLine(r1)
